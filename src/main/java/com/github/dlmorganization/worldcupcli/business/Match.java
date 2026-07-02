6d368bf793593fcc24852cef89097a6b41395708
Daniel <danielgf1620@gmail.com>
2026-07-02 18:53:57 -0300

    Algumas correções e criação da função playMatch()

--- src/main/java/com/github/dlmorganization/worldcupcli/business/Match.java
+++ src/main/java/com/github/dlmorganization/worldcupcli/business/Match.java
@@ -4,10 +4,12 @@ public class Match {
 
     private Team team1;
     private Team team2;
+    private boolean played;
 
     public Match(Team team1, Team team2) {
         this.team1 = team1;
         this.team2 = team2;
+        this.played = false;
     }
 
     public Team getTeam1() {
@@ -16,5 +18,13 @@ public class Match {
 
     public Team getTeam2() {
         return team2;
+    }
+
+    public boolean isPlayed() {
+        return played;
+    }
+
+    public void setPlayed(boolean played) {
+        this.played = played;
     }
 }
--- src/main/java/com/github/dlmorganization/worldcupcli/business/MatchPlayed.java
+++ src/main/java/com/github/dlmorganization/worldcupcli/business/MatchPlayed.java
@@ -3,14 +3,17 @@ package com.github.dlmorganization.worldcupcli.business;
 import com.github.dlmorganization.worldcupcli.utils.Pair;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class MatchPlayed extends Match{
 
     private Pair<Integer, Integer> score;
 
-    public MatchPlayed(Team team1, Team team2, Pair<Integer, Integer> score) {
+    public MatchPlayed(Team team1, Team team2) {
         super(team1, team2);
-        this.score = score;
+        setPlayed(true);
+        Random random = new Random();
+        this.score = new Pair<>(random.nextInt(7), random.nextInt(7));
     }
 
     public Pair<Integer, Integer> getScore() {
